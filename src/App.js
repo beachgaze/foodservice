9269fe7084b75fd84093b74af6cee50b0b32f4ba
jorge velazquez <jorgevelazquez@jorges-MacBook-Air.local>
2021-05-19 17:11:27 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import logo from './logofs.png';
 import './App.css';
+import img from './food.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+   
+
+<img alt='' class="logo" src={logo} />
+
+ <h3 className="title">
+     Inicio │ Registro │ Login │ Configuración │ Pago
+   </h3>
+ 
+
+
+<br/>
+<hr/><hr/>
+
+<br/><br/>
+
+<img alt='ceviche peruano' class="img" src= {img} />
+
+ <h3 className="title">
+     Comprar │ Agregar a carrito │ Poner reseña ★★★★★
+   </h3>
+
+<br/><br/>
+<hr/>
+
+<br/><br/>
+
+</div>
   );
 }
 
 export default App;
+
